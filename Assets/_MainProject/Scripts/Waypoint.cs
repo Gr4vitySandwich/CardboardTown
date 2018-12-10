094d35e33d8bcadbda301cc534e6f02989b3c46f
Rashid <ralnabhan@windowslive.com>
2018-12-10 18:49:35 +0300

    AIPatrol
    AIPatrol

---
+++ Assets/_MainProject/Scripts/Waypoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour
+{
+
+    [SerializeField]
+    float debugDrawRadius = 1.0F;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+    }
+}
